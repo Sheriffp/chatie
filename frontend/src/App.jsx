3cb9c8f9cab9a4ba833829977dbb7dc641dce65d
Sheriff Pascal <103493791+Sheriffp@users.noreply.github.com>
2025-09-26 04:14:31 +0100

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,9 @@ import { Routes, Route } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
-	const { authUser,login } = useAuthStore();
+	
 	return (
 		<div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden'>
 			<div className='absolute top-0 -left-4 size-96 bg-purple-500 opacity-20 blur-[100px]' />
