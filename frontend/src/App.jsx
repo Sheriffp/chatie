a94c4085ed3c2813e22caf372f2d660b525e54c0
Sheriff Pascal <103493791+Sheriffp@users.noreply.github.com>
2025-09-26 04:06:47 +0100

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignupPage from "./pages/SignupPage";
 import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
-	const { authUser,login,isLoggedIn } = useAuthStore();
+	const { authUser,login } = useAuthStore();
 	return (
 		<div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden'>
 			<div className='absolute top-0 -left-4 size-96 bg-purple-500 opacity-20 blur-[100px]' />
